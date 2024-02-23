cc99e8b14ef03307b8e2b7d4c327bdfe48cb7453
Katherin Ortiz <kportiz88@misena.edu.co>
2024-02-23 08:42:37 -0500

    23Feb0842 ⛘ ajuste en colecciones zcache - 11-error-en-funcion-backend-rundrive

--- functions/src/eCommerce/zCache.js
+++ functions/src/eCommerce/zCache.js
@@ -19,6 +19,7 @@ const runZcache = async (fx, parameters) => {
 
   switch (fx) {
   case "signUp":
+    // await runZcache("signUp", `${currentPrivateV0.setup.authDomain}|${targetEntity}|principal|${targetEntity}`);
     /*
       Ejemplo de entrada:
 {
@@ -62,6 +63,35 @@ const runZcache = async (fx, parameters) => {
 
     return Promise.resolve({response: template.response});
 
+  case "{uidX}_templates": {
+    // await runZcache("{uidX}_templates", `${currentPrivateV0.setup.authDomain}|${targetEntity}|principal|${targetEntity}`);
+    // Obtener la plantilla para signUp
+    template = await getOneDocument(`/zcacheTemplate/${fx}`);
+
+    if (template.response === code.ok) {
+      const dataX = {};
+      dataX["uidX"] = paramsX[0];
+
+      // Interpolar campos.
+      const {interpolateString} = require("../admin/utils");
+      const queryPersonalizedTemplates = interpolateString(template.data.queries.queryPersonalizedTemplates.collection, dataX);
+      console.warn("dentro DE SETROLESRUN", JSON.stringify({queryPersonalizedTemplates: queryPersonalizedTemplates, dataX: dataX, parameters: parameters}));
+
+      template.data.queries.queryPersonalizedTemplates.collection = queryPersonalizedTemplates;
+
+      // Ruta para el paquete de caché
+      const bundlePath = `/zcache/${dataX.uidX}_templates`;
+
+      // Establecer TTL para que expire en menos de 72 horas
+      const {timeStampFirestoreX} = require("../admin");
+      template.data["TTL"] = timeStampFirestoreX;
+
+      // Combinar y guardar en Firestore
+      await mergeInFirestore(bundlePath, template.data, false);
+      return Promise.resolve({response: code.created});
+    }
+    return Promise.resolve({response: template.response});
+  }
   case "exist":
     /*
       Ejemplo de entrada:
